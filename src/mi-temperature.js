76fc8f9268ba6e4e4e830a5e5a790e0bdc744c6b
Jukka Pajarinen <jukka.pajarinen@lumitum.com>
2023-02-18 11:59:37 +0200

    Parsing sensor byte values to human readable formats

--- src/mi-temperature.js
+++ src/mi-temperature.js
@@ -14,15 +14,21 @@ async function main() {
 
     console.log("BLE peripheral:", await device.toString());
 
+    // Uuids are checked for LYWSD03MMC with custom ATC firmware
     const batteryService = await gattServer.getPrimaryService("0000180f-0000-1000-8000-00805f9b34fb");
     const sensingService = await gattServer.getPrimaryService("0000181a-0000-1000-8000-00805f9b34fb");
     const batteryCharacteric = await batteryService.getCharacteristic("00002a19-0000-1000-8000-00805f9b34fb");
     const temperatureCharacteric = await sensingService.getCharacteristic("00002a1f-0000-1000-8000-00805f9b34fb");
     const humidityCharacteric = await sensingService.getCharacteristic("00002a6f-0000-1000-8000-00805f9b34fb");
 
-    console.log("Battery:", await batteryCharacteric.readValue());
-    console.log("Temperature:", await temperatureCharacteric.readValue());
-    console.log("Humidity:", await humidityCharacteric.readValue());
+    // Sensory values are advertised as little endian bytes
+    const battery = await (await batteryCharacteric.readValue()).readUInt8();
+    const temperature = await (await temperatureCharacteric.readValue()).readInt16LE() / 10;
+    const humidity = await (await humidityCharacteric.readValue()).readInt16LE() / 100;
+
+    console.log("Battery:", battery);
+    console.log("Temperature:", temperature);
+    console.log("Humidity:", humidity);
   }
 }
 
