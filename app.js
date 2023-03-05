309ee719a96e883e3d376459984431d32297332c
Jukka Pajarinen <jukka.pajarinen@lumitum.com>
2023-03-05 13:58:33 +0200

    Helmet and fixes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import bodyparser from "body-parser";
+import helmet from "helmet";
 import DashboardController from "./controllers/DashboardController.js";
 import SensorsController from "./controllers/SensorsController.js";
 
@@ -12,6 +13,7 @@ async function main() {
   console.log("::: Mi Sensor Monitoring System :::");
   app.set("view engine", "ejs");
   app.set("views", ".");
+  app.use(helmet());
   app.use(bodyparser.urlencoded({extended:true}));
   app.use("/", new DashboardController());
   app.use("/", new SensorsController());
